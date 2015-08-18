c2c129daf45e1755ade152361a6c42d39368b8fa
MTata <matthew.tata@uleth.ca>
2015-08-18 10:12:23 -0600

    Removing old stuff.  It's been moved to /old

--- Behaviour/LoadParameters.m
+++
@@ -1,4 +0,0 @@
-rate=4; %hz...how fast to update this view of the object
-useYARP=1;
-timeToWaitForYarp=3; %seconds
-
--- Behaviour/SendAudioOut/KillAudio.m
+++
@@ -1,7 +0,0 @@
-
-display('done writing audio');
-
-
-%close down neatly
-PsychPortAudio('Stop', paoutput, 1);
-PsychPortAudio('Close');
