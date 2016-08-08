4beaa11732573a050e20816fc84a3897de47c090
milievski <m.ilievski@uleth.ca>
2016-08-08 12:16:20 -0600

    Added check in head angle

--- src/headAngleMemoryMapper/src/headAngleMemoryMapper.cpp
+++ src/headAngleMemoryMapper/src/headAngleMemoryMapper.cpp
@@ -9,7 +9,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/stat.h>
-#include <sys/mman.h> 
+#include <sys/mman.h>
 #include <fcntl.h>
 
 using namespace yarp::os;
@@ -20,16 +20,16 @@ using namespace yarp::dev;
 int main()
 {
   Network yarp;
- 
 
-    Property options;
+
+  Property options;
   options.put("device", "remote_controlboard");
   options.put("local", "/AudioFocusAttention");
   options.put("remote", "/icub/head");
-  
+
   PolyDriver robotHead(options);
- 
-  
+
+
   if (!robotHead.isValid()) {
     printf("Cannot connect to robot head\n");
     return 1;
@@ -37,47 +37,51 @@ int main()
   IPositionControl *pos;
   IVelocityControl *vel;
   IEncoders *enc;
-  
+
   robotHead.view(pos);
   robotHead.view(vel);
   robotHead.view(enc);
-  
-   double* pies  = NULL; // Pointer initialized with null
-   pies  = new double;   // Request memory for the variable
 
-    FILE *fid;
+  double* pies  = NULL; // Pointer initialized with null
+  pies  = new double;   // Request memory for the variable
 
-    fid=fopen("/tmp/headAngleMemMap.tmp","w");
-    fwrite(pies, sizeof(double), sizeof(pies), fid);
-    fclose(fid);
-    int mappedFileID;
-    mappedFileID=open("/tmp/headAngleMemMap.tmp",O_RDWR);
-    double *mappedAudioData;
-    mappedAudioData=(double *)mmap(0,sizeof(double), PROT_WRITE, MAP_SHARED , mappedFileID, 0);
+  FILE *fid;
+
+  fid = fopen("/tmp/headAngleMemMap.tmp", "w");
+  fwrite(pies, sizeof(double), sizeof(pies), fid);
+  fclose(fid);
+  int mappedFileID;
+  mappedFileID = open("/tmp/headAngleMemMap.tmp", O_RDWR);
+  double *mappedAudioData;
+  mappedAudioData = (double *)mmap(0, sizeof(double), PROT_WRITE, MAP_SHARED , mappedFileID, 0);
 
 
-  if (pos==NULL || vel==NULL || enc==NULL) {
+  if (pos == NULL || vel == NULL || enc == NULL) {
     printf("Cannot get interface to robot head\n");
     robotHead.close();
     return 1;
   }
 
-    int jnts = 0;
+  int jnts = 0;
   pos->getAxes(&jnts);
   Vector setpoints;
   setpoints.resize(jnts);
-    Vector checkpoints;
+  Vector checkpoints;
   checkpoints.resize(jnts);
 
-
-  while (1){
+  double oldLocation = 0;
+  while (1) {
     usleep(40000);
+
     enc->getEncoders (checkpoints.data());
-    *mappedAudioData = (double) checkpoints[0];
+    if (oldLocation != (double) checkpoints[0]) {
+      *mappedAudioData = (double) checkpoints[0];
+      oldLocation = (double) checkpoints[0];
+    }
     printf ( "%f \n", *mappedAudioData);
   }
 
 
-return 0;
+  return 0;
 
 }
