a753c923554f1fce718c3f35b69f043b4ceee897
Austin Kothig <kothiga2112@gmail.com>
2017-08-16 15:33:29 -0600

    documented class beamFormer. Changed the way beamformer does multithreading

--- src/audioPreprocessing/include/beamFormer.h
+++ src/audioPreprocessing/include/beamFormer.h
@@ -28,71 +28,96 @@
 #include <thread>
 
 class BeamFormer {
+
 public:
 	/**
-	*	Default constructor
-	*	Calls loadFile()
-	*	Calls makeErbCFs()
-	*	Pre-allocates the data structures that will hold the inputAudio as well as the filteredAudio
-	*	@param file A string that contains the filename location of the initialization file
-	*/
-	BeamFormer(int numBands, int nSamples, int numMics, int getNBeamsPerHemifield);
+	 *	constructor
+	 *	
+	 *	Pre-allocates the data structures that will hold the inputAudio as well as the filteredAudio
+	 *   
+	 *	@param          numBands : number of frequency bands
+	 *	@param          nSamples : sampling rate 
+	 *	@param           numMics : number of microphones
+	 *	@param numBeamsHemifield : number of beams in hemifield
+	 */
+	BeamFormer(int numBands, int nSamples, int numMics, int numBeamsHemifield);
+
 
 	/**
-	*	 Default De-constructor
-	*/
+	 *	destructor
+	 */
 	~BeamFormer();
 
-	/**
-	*	inputAudio
-	*	Sets the audio file that is passed into this file as the audio to be filtered
-	*	TODO make an Error check function that will check if the input audio is valid
-	*	Calls generatFilter()
-	*	@param  inAudio An array which contains audio data. The left channel are all the even indexs and right channels are all the odd indexs.
-	*/
-	void inputAudio(std::vector< float* > inAudio);
 
 	/**
-	*	 getBeamAudio
-	*	 Function that will run multithread function that will calculate the Beam formed Audio from information that is stored in inputSignal.
-	*	 @return Beam fromed data that is stored in vector of size(totalBeams) containing a vector of size(number of Bands) that contains a array of float with a size(samples in frame).
-	*/
-	std::vector<std::vector<float*> > getBeamAudio();
+	 *	inputAudio
+	 *
+	 *	Sets the audio file that is passed into this file as the audio to be filtered
+	 *
+	 *	TODO make an Error check function that will check if the input audio is valid
+	 *
+	 *	@param  inAudio : An array which contains audio data. The left channel are all 
+	 *					  the even indexs and right channels are all the odd indexs.
+	 */
+	void inputAudio(std::vector< float* > inAudio);
+
+
 	/**
-	*	 getReducedBeamAudio
-	*	 Function that will run multithread function that will calculate the Beam formed Audio from information that is stored in inputSignal.
-	*	 This Function then compresses the data in the time frame by running RMS(root mean square).
-	*	 @return Compressed beam formed data that is stored in vector of size(totalBeams) containing a vector of size(number of Bands) that contains the average power in each frame.
-	*/
+	 *	getBeamAudio
+	 *
+	 *	Function that will run multithread function that will calculate the Beamformed 
+	 *	Audio from information that is stored in inputSignal.
+	 *
+	 *	@return Beamformed data that is stored in vector of size(totalBeams) containing a vector of 
+	 * 			size(number of Bands) that contains a array of float with a size(samples in frame).
+	 */
+	std::vector<std::vector<float*> > getBeamAudio();
+
+
+	/**
+	 *	getReducedBeamAudio
+	 *
+	 *	Function that will run multithread function that will calculate the 
+	 *	Beamformed Audio from information that is stored in inputSignal.
+	 *	This Function then compresses the data in the time frame by running RMS(root mean square).
+	 *
+	 *	@return Compressed beamformed data that is stored in vector of size(totalBeams) containing 
+	 *	        a vector of size(number of Bands) that contains the average power in each frame.
+	 */
 	std::vector<std::vector<double> > getReducedBeamAudio();
+
 private:
 
 	/**
-	*	 reducedAudioMultiThreadingLoop
-	*	 A helper function used with the C++11 multithreading to create the reducedAudioMap.
-	*	 @param i .
-	*/
+	 *	reducedAudioMultiThreadingLoop
+	 *
+	 *	A helper function used with the C++11 multithreading to create the reducedAudioMap.
+	 *
+	 *	@param i : the beam that work will be done on
+	 */
 	void reducedAudioMultiThreadingLoop(int i);
 
+
 	/**
-	*	 audioMultiThreadingLoop
-	*	 A helper function used with the C++11 multithreading to create the audioMap.
-	*	 @param i .
-	*/
+	 *	audioMultiThreadingLoop
+	 *
+	 *	A helper function used with the C++11 multithreading to create the audioMap.
+	 *
+	 *	@param i : the beam that work will be done on
+	 */
 	void audioMultiThreadingLoop(int i);
 
 
-	std::vector< float* > inputSignal;										//The input audio signal
-	std::vector < std::vector < float* > > beamFormedAudioVector;			//The uncompressed beamformed data if getBeamAudio() was called
-	std::vector < std::vector < double > > reducedBeamFormedAudioVector;	//The compressed beamformed data if getReducedBeamAudio() was called
-	
-	int nMics;																//Number of Microphones that is used
-	int frameSamples;														//The number of samples in each frame of audio data
-	int nBands;																//The maximum number of bands for the given spacing of the mics
+	int nMics;						// Number of Microphones that is used
+	int frameSamples;				// The number of samples in each frame of audio data
+	int nBands;						// The maximum number of bands for the given spacing of the mics
 
-	int getNBeamsPerHemifield;												//The maximum number of bands in each hemifield
-	int totalBeams;															//Total number of beams that is used
+	int getNBeamsPerHemifield;		// The maximum number of bands in each hemifield
+	int totalBeams;					// Total number of beams that is used
 
+	std::vector< float* > inputSignal;										// The input audio signal
+	std::vector < std::vector < float* > > beamFormedAudioVector;			// The uncompressed beamformed data if getBeamAudio() was called
+	std::vector < std::vector < double > > reducedBeamFormedAudioVector;	// The compressed beamformed data if getReducedBeamAudio() was called
 };
 
 #endif
--- src/audioPreprocessing/src/beamFormer.cc
+++ src/audioPreprocessing/src/beamFormer.cc
@@ -23,62 +23,87 @@ inline int myMod(int a, int b) {
 	return  a >= 0 ? a % b : (a % b) + b;
 }
 
+
 BeamFormer::BeamFormer(int numBands, int nSamples, int numMics, int numBeamsHemifield):
-nMics(numMics), frameSamples(nSamples), nBands(numBands), getNBeamsPerHemifield(numBeamsHemifield) 
-{
+nMics(numMics), frameSamples(nSamples), nBands(numBands), getNBeamsPerHemifield(numBeamsHemifield) {
+	
 	totalBeams = (getNBeamsPerHemifield*2) + 1;
 	
 	//Allocating the required vectors to create the beamFormed audio 
-	for (int i = 0; i < totalBeams; i++)
-	{
+	for (int i = 0; i < totalBeams; i++) {
+
 		std::vector<float*> tempvector;
-		for (int j = 0; j < nBands; j++)
-		{
+
+		for (int j = 0; j < nBands; j++) {
 			float* temparray = new float[frameSamples];
 			tempvector.push_back(temparray);
 		}
+
 		beamFormedAudioVector.push_back(tempvector);
 	}
+
 	//Allocating the required vectors to create the reduced beamFormed audio 
-	for (int i = 0; i < totalBeams; i++)
-	{
+	for (int i = 0; i < totalBeams; i++) {
+
 		std::vector<double> tempvector;
-		for (int j = 0; j < nBands; j++)
-		{
+		
+		for (int j = 0; j < nBands; j++) {
 			double tempnumber = 0;
 			tempvector.push_back(tempnumber);
 		}
+
 		reducedBeamFormedAudioVector.push_back(tempvector);
 	}
-
 }
+
 
 BeamFormer::~BeamFormer()
 {
-	for(int i = 0; i < inputSignal.size();i++){
+	for(int i = 0; i < inputSignal.size();i++) {
 		delete[] inputSignal[i];
 	}
-	for(int i = 0; i < beamFormedAudioVector.size();i++){
+
+	for(int i = 0; i < beamFormedAudioVector.size();i++) {
 		for(int j = 0; j < beamFormedAudioVector[i].size();j++)
-		delete[] beamFormedAudioVector[i][j];
+			delete[] beamFormedAudioVector[i][j];
 	}
 }
 
-void BeamFormer::inputAudio(std::vector< float* > inAudio)
-{
+
+void BeamFormer::inputAudio(std::vector< float* > inAudio) {
 	//Sets the input audio passed into this function as inAudio.
 	inputSignal = inAudio;
 }
 
-std::vector<std::vector<float*> > BeamFormer::getBeamAudio()
-{
-	std::vector<std::thread> myThread;
-	//Starting all the threads
-	for (int i = 0; i < totalBeams; i++)
-	{
-		myThread.push_back(std::thread(&BeamFormer::audioMultiThreadingLoop, this, i));
 
+std::vector<std::vector<float*> > BeamFormer::getBeamAudio() {
+
+	int i = 0, j = 0, limit = std::thread::hardware_concurrency() * 4;
+
+	std::vector<std::thread> myThread(limit);
+	
+	while (i < totalBeams) {
+		for ( ; i < totalBeams; i++) {
+
+			if (i-j > limit) break;
+
+			myThread[i-j] = std::thread(&BeamFormer::audioMultiThreadingLoop, this, i);
+		}
+
+		
+		for (int k = 0; j < i; j++, k++) 
+			myThread[k].join();
 	}
+
+	/* OLD CODE
+	std::vector<std::thread> myThread;
+
+	//Starting all the threads
+	for (int i = 0; i < totalBeams; i++) {
+		myThread.push_back(std::thread(&BeamFormer::audioMultiThreadingLoop, this, i));
+	}
+
+	
 	auto othread = myThread.begin();
 	//TODO This is not a good way of joining (this method is far too slow)
 	while (othread != myThread.end())
@@ -86,12 +111,33 @@ std::vector<std::vector<float*> > BeamFormer::getBeamAudio()
 		othread->join();
 		othread++;
 	}
+	*/
 
 	return beamFormedAudioVector;
 }
 
-std::vector<std::vector<double> > BeamFormer::getReducedBeamAudio()
-{
+
+std::vector<std::vector<double> > BeamFormer::getReducedBeamAudio() {
+
+	int i = 0, j = 0, limit = std::thread::hardware_concurrency() * 4;
+
+	std::vector<std::thread> myThread(limit);
+	
+	while (i < totalBeams) {
+		for ( ; i < totalBeams; i++) {
+
+			if (i-j > limit) break;
+
+			myThread[i-j] = std::thread(&BeamFormer::reducedAudioMultiThreadingLoop, this, i);
+		}
+
+		
+		for (int k = 0; j < i; j++, k++) 
+			myThread[k].join();
+	}
+
+
+	/* 	OLD CODE
 	std::vector<std::thread> myThread;
 	//Starting all the threads
 	for (int i = 0; i < totalBeams; i++)
@@ -106,31 +152,29 @@ std::vector<std::vector<double> > BeamFormer::getReducedBeamAudio()
 		othread->join();
 		othread++;
 	}
+	*/
 
 	return reducedBeamFormedAudioVector;
 }
 
+
 void BeamFormer::audioMultiThreadingLoop(int i) {
 
 
-	for (int j = 0; j < nBands; j++)
-	{
-		for (int k = 0; k < frameSamples; k++)
-		{
+	for (int j = 0; j < nBands; j++) {
+		for (int k = 0; k < frameSamples; k++) {
 			beamFormedAudioVector[i][j][k] = (inputSignal[j][k] + inputSignal[j + nBands][myMod(k + ((getNBeamsPerHemifield) - i), frameSamples)]);
 		}
 	}
-
 }
+
+
 void BeamFormer::reducedAudioMultiThreadingLoop(int i) {
 
-	for (int j = 0; j < nBands; j++)
-	{
-		for (int k = 0; k < frameSamples; k++)
-		{
+	for (int j = 0; j < nBands; j++) {
+		for (int k = 0; k < frameSamples; k++) {
 			reducedBeamFormedAudioVector[i][j] += pow((inputSignal[j][k] + inputSignal[j + nBands][myMod(k + ((getNBeamsPerHemifield) - i), frameSamples)]), 2);
 		}
 		reducedBeamFormedAudioVector[i][j] = sqrt((static_cast<double>(1) / frameSamples) * (reducedBeamFormedAudioVector[i][j]));
 	}
-
 }
