e5672d36fa112d9cd664c052cb7abe501b4ca7bd
milievski <ilievski.mm@gmail.com>
2017-09-30 23:03:46 -0400

    Added visualization for raw audio

--- MATLAB/Visualization/visualizationRawAudio.m
+++ MATLAB/Visualization/visualizationRawAudio.m
@@ -4,6 +4,6 @@ f=dir(memMapFileName);
 audioInv2  = memmapfile(memMapFileName, 'Writable', false, 'format',{'double' [2 4800] 'sound'});
 
 while(1)
-    plot((audioInv2.Data(1,1).sound(1,:)));
+    plot(linspace(0,1,4800),(audioInv2.Data(1,1).sound(1,:)));
     drawnow;
 end
