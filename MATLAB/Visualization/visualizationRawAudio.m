3c11539711d6090a9a2b0a87a6466e2f80b7cff1
milievski <ilievski.mm@gmail.com>
2017-09-30 23:13:49 -0400

    Changed mem mapper to enforce double checking if this fixed bug

--- MATLAB/Visualization/visualizationRawAudio.m
+++ MATLAB/Visualization/visualizationRawAudio.m
@@ -1,7 +1,7 @@
 %this memory mapped raw Audio
 memMapFileName='/tmp/rawAudio.tmp';
 f=dir(memMapFileName);
-audioInv2  = memmapfile(memMapFileName, 'Writable', false, 'format',{'double' [2 4800] 'sound'});
+audioInv2  = memmapfile(memMapFileName, 'Writable', false, 'format',{'double' [4800 2] 'sound'});
 
 while(1)
     plot(linspace(0,1,4800),(audioInv2.Data(1,1).sound(1,:)));
