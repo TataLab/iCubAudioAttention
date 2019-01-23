e1d04f6fd0763e49a04fca7e9e711da1545431f7
kothiga <kothiga2112@gmail.com>
2019-01-23 13:57:37 -0700

    dev of audioplayer

--- src/audioExperiment/audioPlayer/audioPlayer.py
+++ src/audioExperiment/audioPlayer/audioPlayer.py
@@ -1,10 +1,12 @@
 import numpy as np
-from scipy import interpolate
-from scipy.signal import hilbert
+import librosa
+import soundfile as sf
 import matplotlib.pyplot as plt
+
+import csv
+import os
 import pyaudio
 import yarp
-import csv
 
 import argparse
 
@@ -13,9 +15,9 @@ yarp.Network.init()
 
 def get_args():
     parser = argparse.ArgumentParser(description='player')
-    parser.add_argument('--name',  default="/audioPlayer",    help='Name for the module.                 (default: {})'.format("audioPlayer"))
-    parser.add_argument('--rate',  default=48000, type=int,  help='Sampling rate.                       (default: {})'.format(48000))
-    parser.add_argument('--nchan', default=2,     type=int,  help='Number of Audio Channels in Array.   (default: {})'.format(2))
+    parser.add_argument('--name',  default="/audioPlayer",  help='Name for the module.                 (default: {})'.format("audioPlayer"))
+    parser.add_argument('--rate',  default=48000, type=int, help='Sampling rate.                       (default: {})'.format(48000))
+    parser.add_argument('--nchan', default=2,     type=int, help='Number of Audio Channels in Array.   (default: {})'.format(2))
     
     args = parser.parse_args()
     return args
@@ -25,12 +27,16 @@ class audioPlayer(object):
 
     def __init__(self, args):
 
-        # Parse the Arguments.
+        # Init the Yarp Port.
         self.port_name = args.name
 
-        self.numChannels  = args.nchan
+        self.input_port = yarp.RpcServer()
+        self.input_port.open(self.port_name + "/rpc:i")
+
+
+        # Init PortAudio Device.
         self.samplingRate = args.rate
-        
+        self.numChannels  = args.nchan
 
         # For paFloat32 sample values must be in range [-1.0, 1.0]
         self.audioDevice = pyaudio.PyAudio()
@@ -41,12 +47,7 @@ class audioPlayer(object):
             output=True
         )
         
-        
-
-
-        self.input_port = yarp.RpcServer()
-        self.input_port.open(self.port_name + "/rpc:i")
-
+        # Read in the CSV containing trial information.
         #reader = csv.reader(open(csv_filename, "rb"), delimiter=",")
         #data   = list(reader)
         #print(data)
@@ -67,27 +68,78 @@ class audioPlayer(object):
             cmd = command.get(0).asString()
             len = command.size()
 
-            if cmd == "quit":
+            # Kill this script.
+            if cmd == "quit" or cmd == "--quit" or cmd == "-q" or cmd == "-Q":
                 reply.addString("Good Bye!")
                 self.input_port.reply(reply)
                 return
 
-            elif cmd == "help":
-                reply.addString("Commands: trial <int> | play <-1:all or {}>  <string> | quit".format(list(ch for ch in range(self.numChannels))))
+            # Get a help Message.
+            elif cmd == "help" or cmd == "--help" or cmd == "-h" or cmd == "-H":
+                reply.addString( # Please don't look at this.
+                    "Commands: trial <int> | play -1 <string>.wav | play {} | ping | quit ".format(
+                        " ".join(list(" ".join([str(ch), '{}.wav'.format(chr(97+ch))]) for ch in range(self.numChannels)))
+                    )
+                )
             
-            elif cmd == "trial":
-                reply.addString("please trial me!")
-                self.trial(0)
+            # Ping the server.
+            elif cmd == "ping" or cmd == "--ping":
+                reply.addString("Oll Korrect")
+
+            # Run a trial from the provided csv.
+            elif cmd == "trial" or cmd == "--trial" or cmd == "-t" or cmd == "-T":
+                if len > 2:
+                    reply.addString("Too many commands. Message should be ``trial <int>`` not ``{}``".format(command.toString()))
+                elif len == 1:
+                    reply.addString("Please indicate the trial number. Message should be ``trial <int>``")
+                else:
+                    # Check that trial number is valid.
+                    trial_num = command.get(1).asInt()
+                    if trial_num == 0:
+                        reply.addString("Please ensure the trial number is an integer, not string ``{}``".format(command.get(1).asString()))
+                    else:
+                        self.trial(trial_num)
+                        reply.addString("Trial Complete")
             
-            elif cmd == "play":
-                reply.addString("please play me!")
-                self.play(-1, "None")
-            
+            # Play an instance of 
+            elif cmd == "play" or cmd == "--play" or cmd == "-p" or cmd == "-P":
+                if len % 2 == 0 or len == 1:
+                    reply.addString("play command should be used as ``play -1 <string>`` or ``play <int> <string> <int> <string>``")
+                else:
+                    play_at  = []
+                    play_wav = []
+                    play_msg = ""
+                    play_success = False
+
+                    # Play at All Channels.
+                    if command.get(1).asInt() == -1:
+                        if len == 3:
+                            play_success, play_msg = self.play(-1, command.get(2).asString())
+                        else:
+                            reply.addString("Too many arguments for ``play -1 <string>``")
+
+                    # Play at Specific Channels.
+                    else:
+                        for idx in range(1, len, 2):
+                            play_at.append(command.get(idx).asInt())
+                            play_wav.append(command.get(idx+1).asString())
+                        play_success, play_msg = self.play(play_at, play_wav)
+
+                    # Prep the response.
+                    if play_success:
+                        reply.addString("Play Complete")
+                    else:
+                        reply.addString("Error in Play") 
+                        reply.addString("PlayError: {}".format(play_msg))
+
+
+            # Catch Unknown Commands.
             elif len == 0: continue
             else:
                 reply.addString("Unkown")
                 reply.addString("Command")
                 reply.append(command)
+
 
             # Reply back on the rpc port.
             self.input_port.reply(reply)
@@ -99,10 +151,51 @@ class audioPlayer(object):
         return
 
     
-    def play(self, ch, wav_file):
-        print("Begin Playing {} on channel {}".format(wav_file, ch))
-        return
+    def play(self, channels, filename):
+        print("Begin Playing {} on channel {}".format(filename, channels))
 
+        if channels == -1:
+            if not os.path.isfile(filename):
+                return False, "Audio File {} does not exist.".format(filename)
+            
+            data, samps = self.open_audio(filename)
+            audioSamples = np.zeros((self.numChannels, samps), dtype=np.float32)
+            audioSamples[:] = data
+        
+        else:
+            if len(channels) > len(set(channels)):
+                return False, "The provided channels should be unique. Was given {}".format(channels)
+
+            maxNumSamps  = 1
+            audioSamples = np.zeros((self.numChannels, maxNumSamps), dtype=np.float32)
+
+            for ch, fn in zip(channels, filename):
+                if ch >= self.numChannels or ch < 0:
+                    return False, "Channel {} is out of range [0:{}]".format(ch, self.numChannels)
+                if not os.path.isfile(fn):
+                    return False, "Audio File {} does not exist.".format(fn)
+                
+                data, samps = self.open_audio(fn)
+
+                # Resize the audioSamples if we load a file that is longer.
+                if samps > maxNumSamps: 
+                    resize_samples = np.zeros((self.numChannels, samps), dtype=np.float32)
+                    resize_samples[:,:-(samps-maxNumSamps)] = audioSamples
+                    audioSamples = resize_samples
+                    maxNumSamps  = samps
+
+                audioSamples[ch,:samps] = data
+
+        # Write to stream.
+        self.audioStream.write(audioSamples.transpose().tobytes())
+        return True, ""
+
+
+    def open_audio(self, filename):
+        data, rate = sf.read(filename)
+        if rate != self.samplingRate:
+            data = librosa.resample(data, rate, self.samplingRate)
+        return data, data.shape[0]
 
 
     def cleanup(self):
