e34fda68cb9c641b89e8ae0b60c0181e7375c97c
kothiga <kothiga2112@gmail.com>
2019-03-25 12:11:29 -0600

    touch ups to complex script.

--- Python/complexAudio/generateComplexAudio.py
+++ Python/complexAudio/generateComplexAudio.py
@@ -1,6 +1,4 @@
 import numpy as np
-import scipy.signal as sig
-from scipy.signal import butter, lfilter, freqz
 import soundfile as sf
 
 import os
@@ -8,15 +6,15 @@ import argparse
 
 def get_args():
     parser = argparse.ArgumentParser(description='complex')
-    parser.add_argument('-r', '--rate',  default=48000,  type=int,           help='Sampling Rate of Audio.               (default: {})'.format(48000))
-    parser.add_argument('-t', '--time',  default=15.0,   type=float,         help='Time in seconds to generate.          (default: {})'.format(15.0))
-    parser.add_argument('-m', '--mod',   default=5.0,    type=float,         help='Frequency to modulate at.             (default: {})'.format(5.0))
-    parser.add_argument('-b', '--band',  default=64,     type=int,           help='Number of Center Frequencies.         (default: {})'.format(64))
-    parser.add_argument('-L', '--low',   default=300.0,  type=float,         help='Low Center Frequency.                 (default: {})'.format(300.0))
-    parser.add_argument('-H', '--high',  default=4800.0, type=float,         help='High Center Frequency.                (default: {})'.format(4800.0))
-    parser.add_argument('-s', '--space', default=2,      type=int,           help='Frequency Band Spacing Used.          (default: {})'.format(2))
-    parser.add_argument('-l', '--lin',   default=False, action='store_true', help='Make Linear Spaced.                   (default: {})'.format(False))
-    parser.add_argument('-S', '--save',  default='output.wav',               help='Name of the output file.              (default: {})'.format('output.wav'))
+    parser.add_argument('-r', '--rate',  default=48000,  type=int,            help='Sampling Rate of Audio.               (default: {})'.format(48000))
+    parser.add_argument('-t', '--time',  default=15.0,   type=float,          help='Time in seconds to generate.          (default: {})'.format(15.0))
+    parser.add_argument('-m', '--mod',   default=5.0,    type=float,          help='Frequency to modulate at.             (default: {})'.format(5.0))
+    parser.add_argument('-b', '--band',  default=64,     type=int,            help='Number of Center Frequencies.         (default: {})'.format(64))
+    parser.add_argument('-L', '--low',   default=300.0,  type=float,          help='Low Center Frequency.                 (default: {})'.format(300.0))
+    parser.add_argument('-H', '--high',  default=4800.0, type=float,          help='High Center Frequency.                (default: {})'.format(4800.0))
+    parser.add_argument('-s', '--space', default=2,      type=int,            help='Frequency Band Spacing Used.          (default: {})'.format(2))
+    parser.add_argument('-l', '--lin',   default=False,  action='store_true', help='Make Linear Spaced.                   (default: {})'.format(False))
+    parser.add_argument('-S', '--save',  default='output.wav',                help='Name of the output file.              (default: {})'.format('output.wav'))
     args = parser.parse_args()
     return args
 
@@ -37,9 +35,6 @@ def ampModulate(source, freq, rate):
     e = np.sin(freq * t) + 1
     return e * source
 
-def rms(data):
-    return np.sqrt(np.mean(data ** 2))
-
 def HzToErb(Hz):
     return ( 24.7 * ( 0.00437 * Hz + 1.0 ) )
 
@@ -47,10 +42,8 @@ def HzToErbRate( Hz):
     return ( 21.4 * np.log10( 0.00437 * Hz + 1.0 ) )
 
 def ErbRateToHz(Erb):
-    #(pow(10, Erb / 21.4) - 1) / 0.00437;
     return ((10**(Erb / 21.4)) - 1.0) / 0.00437
   
-
 def makeErbCFs(numBands, spacing, lowCf, highCf):
 
     #-- Make sure space is allocated for the 
@@ -95,10 +88,8 @@ def makeLinearCFs(numBands, spacing, lowCf, highCf):
 
 def main():
     args = get_args()
-    if args.lin:
-        cFreq = makeLinearCFs(args.band, args.space, args.low, args.high)
-    else:
-        cFreq = makeErbCFs(args.band, args.space, args.low, args.high)
+    if args.lin: cFreq = makeLinearCFs(args.band, args.space, args.low, args.high)
+    else:        cFreq = makeErbCFs(args.band, args.space, args.low, args.high)
 
     tone    = genComplex(cFreq, args.rate, args.time)
     ampTone = ampModulate(tone, args.mod, args.rate)
