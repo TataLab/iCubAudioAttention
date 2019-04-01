fed964b080dee1c71da6abedba6108801401cc6e
kothiga <kothiga2112@gmail.com>
2019-04-01 10:14:42 -0600

    added a bash script for running all recorded trials.

---
+++ src/audioExperiment/audioRunner/python/run_all.sh
@@ -0,0 +1,4 @@
+python audioRunner.py -f 32768 -O 16384 -m -d data/recorded/env_01b_2048 -s data/processed/env_01b -c
+python audioRunner.py -f 32768 -O 16384 -m -d data/recorded/env_02b_2048 -s data/processed/env_02b -c
+python audioRunner.py -f 32768 -O 16384 -m -d data/recorded/env_03b_2048 -s data/processed/env_03b -c
+
