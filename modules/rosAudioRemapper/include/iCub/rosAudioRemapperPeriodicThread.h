6b4898ae61e3c9860210650fe237469e8f50c6d5
lukas <lukas.grasse@uleth.ca>
2019-11-20 05:02:29 -0800

    added audio remapper

---
+++ modules/rosAudioRemapper/include/iCub/rosAudioRemapperPeriodicThread.h
@@ -0,0 +1,188 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/*
+ * Copyright (C) 2019 Department of Neuroscience - University of Lethbridge
+ * Author: Austin Kothig, Francesco Rea, Marko Ilievski, Matt Tata
+ * email: kothiga@uleth.ca, francesco.reak@iit.it, marko.ilievski@uwaterloo.ca, matthew.tata@uleth.ca
+ * 
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+/* ===========================================================================
+ * @file  RosAudioRemapperPeriodicThread.h
+ * @brief Definition of a periodic thread that receives raw audio data 
+ *          from an input port and applies various signal processing to it.
+ * =========================================================================== */
+
+#ifndef _ROS_AUDIO_REMAPPER_PERIODICTHREAD_H_
+#define _ROS_AUDIO_REMAPPER_PERIODICTHREAD_H_
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <time.h>
+
+#include <yarp/math/Math.h>
+#include <yarp/sig/all.h>
+#include <yarp/os/all.h>
+#include <yarp/dev/all.h>
+#include <yarp/os/PeriodicThread.h>
+#include <yarp/os/Log.h>
+
+typedef yarp::os::BufferedPort< yarp::sig::Sound  > ySoundBuffer;
+
+class RosAudioRemapperPeriodicThread : public yarp::os::PeriodicThread {
+
+private:
+
+	bool result;                //-- Result of the remapping.
+
+	std::string name;           //-- Rootname of all the ports opened by this thread.
+	std::string robot;          //-- Name of the robot.
+	std::string configFile;     //-- Name of the configFile where the parameter of the camera are set.
+	std::string inputPortName;  //-- Name of input port for incoming events, typically from aexGrabber.
+
+	yarp::os::Stamp timeStamp;  //-- Time stamp updated by yarp network.
+
+	double startTime;           //-- Used for keeping time and reporting temporal
+    double stopTime;            //-- events to the user via command line.
+	
+	double timeDelay,        totalDelay;        //-- Hold on to and store time
+	double timeReading,      totalReading;	    //-- events for clean display
+	double timeProcessing,   totalProcessing;   //-- at the end of a loop.
+	double timeTransmission, totalTransmission; //-- Include stats on the average execution
+	double timeTotal,        totalTime;         //-- when the RFModule is closed.
+
+	int    totalIterations;
+
+
+	/* ===========================================================================
+	 *  Yarp Ports for Sending and Receiving Data from this Periodic Thread.
+	 * =========================================================================== */
+	ySoundBuffer  inRosAudioPort;
+	ySoundBuffer outRawAudioPort;
+
+
+	/* ===========================================================================
+	 *  Yarp Matrices used for Modules Computation. 
+	 *    Objects passed around to encapsulated objects.
+	 * =========================================================================== */
+	yarp::sig::Sound* inputSound;
+	yarp::sig::Sound* outputSound;
+	
+
+	/* ===========================================================================
+	 *  Variables received from the resource finder.
+	 * =========================================================================== */
+	int    azimuthIndex;
+
+
+public:
+	/* ===========================================================================
+	 *  Default Constructor.
+	 * =========================================================================== */
+	RosAudioRemapperPeriodicThread();
+
+
+	/* ===========================================================================
+	 *  Main Constructor.
+	 * 
+	 * @param robotname  : Name of the robot.
+	 * @param configFile : Path to the .ini configuration file.
+	 * =========================================================================== */
+	RosAudioRemapperPeriodicThread(std::string robotname, std::string configFile);
+
+
+	/* ===========================================================================
+	 *  Destructor.
+	 * =========================================================================== */
+	~RosAudioRemapperPeriodicThread();
+
+
+	/* ===========================================================================
+	 *  Configure all the parameters and return true if successful.
+	 * 
+	 * @param rf : Reference to the resource finder.
+	 * 
+	 * @return Flag for the success.
+	 * =========================================================================== */
+	bool configure(yarp::os::ResourceFinder &rf);
+
+
+	/* ===========================================================================
+	 *  Initialises the thread. Build tables and open ports.
+	 * =========================================================================== */
+	bool threadInit();
+
+
+	/* ===========================================================================
+	 *  Correctly releases the thread.
+	 * =========================================================================== */
+	void threadRelease();
+
+
+	/* ===========================================================================
+	 *  Function that sets the rootname of all the ports 
+	 *    that are going to be created by the thread.
+	 * 
+	 * @param str : rootname.
+	 * =========================================================================== */
+	void setName(std::string str);
+	
+
+	/* ===========================================================================
+	 *  Function that returns the original root name and 
+	 *    appends another string iff passed as parameter.
+	 * 
+	 * @param p : pointer to the string that has to be added.
+	 * 
+	 * @return rootname + appended string.
+	 * =========================================================================== */
+	std::string getName(const char* p);
+
+
+	/* ===========================================================================
+	 *  Function that sets the inputPort name.
+	 * =========================================================================== */
+	void setInputPortName(std::string inpPrtName);
+
+
+	/* ===========================================================================
+	 *  Active part of the thread.
+	 * =========================================================================== */
+	void run(); 
+
+
+	/* ===========================================================================
+	 *  Method for the processing in the PeriodicThread.
+	 * =========================================================================== */
+	bool processing();
+
+
+private:
+	
+	/* ===========================================================================
+	 *  Write data to out going ports if something is connected.
+	 * =========================================================================== */
+	void publishOutPorts();
+
+
+	/* ===========================================================================
+	 *  Produce execution stats when the thread is interrupted.
+	 * =========================================================================== */
+	void endOfProcessingStats();
+};
+
+#endif  //_ROS_AUDIO_REMAPPER_PERIODICTHREAD_H_
+
+//----- end-of-file --- ( next line intentionally left blank ) ------------------
---
+++ modules/rosAudioRemapper/src/rosAudioRemapperPeriodicThread.cpp
@@ -0,0 +1,167 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/*
+ * Copyright (C) 2019 Department of Neuroscience - University of Lethbridge
+ * Author: Lukas Grasse, Austin Kothig, Francesco Rea, Marko Ilievski, Matt Tata
+ * email: lukas.grasse@uleth.ca, kothiga@uleth.ca, francesco.reak@iit.it, marko.ilievski@uwaterloo.ca, matthew.tata@uleth.ca
+ * 
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+/* ===========================================================================
+ * @file  rosAudioRemapperPeriodicThread.cpp
+ * @brief Implementation of the rosAudioRemapperPeriodicThread (see header file).
+ * =========================================================================== */
+
+#include <iCub/rosAudioRemapperPeriodicThread.h>
+
+#define THPERIOD 0.01 // seconds.
+
+RosAudioRemapperPeriodicThread::RosAudioRemapperPeriodicThread() : 
+	PeriodicThread(THPERIOD) {
+
+	robot = "icub";
+	startTime = yarp::os::Time::now();        
+}
+
+
+RosAudioRemapperPeriodicThread::RosAudioRemapperPeriodicThread(std::string _robot, std::string _configFile) : 
+	PeriodicThread(THPERIOD) {
+
+	robot = _robot;
+	configFile = _configFile;
+	startTime = yarp::os::Time::now();
+}
+
+
+RosAudioRemapperPeriodicThread::~RosAudioRemapperPeriodicThread() {
+
+}
+
+
+bool RosAudioRemapperPeriodicThread::configure(yarp::os::ResourceFinder &rf) {
+
+	/* ===========================================================================
+	 *  Pull variables for this module from the resource finder.
+	 * =========================================================================== */
+	yInfo( "Loading Configuration File." );
+	
+	/* ===========================================================================
+	 *  Initialize time counters to zero.
+	 * =========================================================================== */
+	totalDelay        = 0.0;
+	totalReading      = 0.0;
+	totalProcessing   = 0.0;
+	totalTransmission = 0.0;
+	totalTime         = 0.0;
+	totalIterations   = 0;
+
+	return true;
+}
+
+
+bool RosAudioRemapperPeriodicThread::threadInit() {
+
+	/* ===========================================================================
+	 *  Initialize all ports. If any fail to open, return false to 
+	 *    let RFModule know initialization was unsuccessful.
+	 * =========================================================================== */
+
+	if (!inRosAudioPort.open(getName("/rosAudio:i").c_str())) {
+		yError("Unable to open port for receiving raw audio from ROS.");
+		return false;
+	}
+
+	if (!outRawAudioPort.open(getName("/rawAudio:o").c_str())) {
+		yError("Unable to open port for sending the output audio.");
+		return false;
+	}
+
+	stopTime = yarp::os::Time::now();
+	yInfo("Initialization of the processing thread correctly ended. Elapsed Time: %f.", stopTime - startTime);
+	startTime = stopTime;
+
+	return true;
+}
+
+
+void RosAudioRemapperPeriodicThread::threadRelease() {
+
+	//-- Stop all threads.
+	inRosAudioPort.interrupt();
+	outRawAudioPort.interrupt();
+
+	//-- Print thread stats.
+	endOfProcessingStats();	
+}
+
+
+void RosAudioRemapperPeriodicThread::setName(std::string str) {
+	this->name=str;
+}
+
+
+std::string RosAudioRemapperPeriodicThread::getName(const char* p) {
+	std::string str(name);
+	str.append(p);
+	return str;
+}
+
+
+void RosAudioRemapperPeriodicThread::setInputPortName(std::string InpPort) {
+	//-- Do nothing.
+}
+
+
+void RosAudioRemapperPeriodicThread::run() {    
+	
+	if (inRosAudioPort.getInputCount()) {
+
+		//-- Get Input.
+		inputSound = inRosAudioPort.read(true);
+		inRosAudioPort.getEnvelope(timeStamp);
+
+				//-- Write to Active Ports.
+		if (outRawAudioPort.getOutputCount()) {
+
+			//-- This Matrix can be very big. Down sample if enabled.
+			outRawAudioPort.setEnvelope(timeStamp);
+			outRawAudioPort.write();
+		}
+
+		//-- Give time stats to the user.
+		timeTotal  = timeDelay + timeReading + timeProcessing + timeTransmission;
+		totalTime += timeTotal;
+		totalIterations++;
+		yInfo("End of Loop %d, TS %d: Delay  %f  |  Reading  %f  |  Processing  %f  |  Transmission  %f  |  Total  %f  |", totalIterations, timeStamp.getCount(), timeDelay, timeReading, timeProcessing, timeTransmission, timeTotal);
+	}
+}
+
+void RosAudioRemapperPeriodicThread::endOfProcessingStats() {
+
+	//-- Display Execution stats.
+	yInfo(" ");
+	yInfo("End of Thread . . . ");
+	yInfo(" ");
+	yInfo("\t Total Iterations : %d", totalIterations);
+	yInfo("\t Total Time       : %.2f", totalTime);
+	yInfo(" ");
+	yInfo("Average Stats . . . ");
+	yInfo(" ");
+	yInfo("\t Delay        : %f", totalDelay        / (double) totalIterations );
+	yInfo("\t Reading      : %f", totalReading      / (double) totalIterations );
+	yInfo("\t Processing   : %f", totalProcessing   / (double) totalIterations );
+	yInfo("\t Transmission : %f", totalTransmission / (double) totalIterations );
+	yInfo("\t Loop Time    : %f", totalTime         / (double) totalIterations );
+	yInfo(" ");
+}
